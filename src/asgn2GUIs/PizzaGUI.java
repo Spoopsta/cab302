e1e8800f7cf2078db1110d3b37db2274818f963b
Spoopsta <nicka98@live.com.au>
2017-05-17 14:53:58 +1000

    Started pizzaGUI.java
    Created basic frame with button that opens a file explorer.

--- src/asgn2GUIs/PizzaGUI.java
+++ src/asgn2GUIs/PizzaGUI.java
@@ -7,34 +7,40 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.text.DecimalFormat;
 
-import javax.swing.JPanel;
 import javax.swing.text.DefaultCaret;
 
 import asgn2Customers.Customer;
 import asgn2Pizzas.Pizza;
 import asgn2Restaurant.PizzaRestaurant;
 
-import javax.swing.JFrame;
-
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 
 /**
  * This class is the graphical user interface for the rest of the system. 
- * Currently it is a ‘dummy’ class which extends JFrame and implements Runnable and ActionLister. 
+ * Currently it is a â€˜dummyâ€™ class which extends JFrame and implements Runnable and ActionLister. 
  * It should contain an instance of an asgn2Restaurant.PizzaRestaurant object which you can use to 
  * interact with the rest of the system. You may choose to implement this class as you like, including changing 
- * its class signature – as long as it  maintains its core responsibility of acting as a GUI for the rest of the system. 
+ * its class signature â€“ as long as it  maintains its core responsibility of acting as a GUI for the rest of the system. 
  * You can also use this class and asgn2Wizards.PizzaWizard to test your system as a whole
  * 
  * 
- * @author Person A and Person B
+ * @author Nicholas Stolberg and Niall Stone
  *
  */
-public class PizzaGUI extends javax.swing.JFrame implements Runnable, ActionListener {
+public class PizzaGUI extends javax.swing.JFrame implements Runnable, ActionListener {	
 	
+	private static final long serialVersionUID = -7031008862559936404L;
+	public static final int WIDTH = 900;
+	public static final int HEIGHT = 700;
 	
+	private JPanel btnPanel;
+	
+	private JButton loadLogFileBTN;
+	private JFileChooser chooser = new JFileChooser();
+		
 	private PizzaRestaurant restaurant;
 	
 	/**
@@ -43,12 +49,101 @@ public class PizzaGUI extends javax.swing.JFrame implements Runnable, ActionList
 	 */
 	public PizzaGUI(String title) {
 		// TO DO
+		super(title);
 	}
-
+	
+	private void createGUI(){
+		this.setSize(WIDTH, HEIGHT);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+		
+		btnPanel = createPanel(Color.GRAY);
+		this.getContentPane().add(btnPanel,BorderLayout.SOUTH);
+		
+		loadLogFileBTN = createButton("Load Log File");
+		
+		layoutButtonPanel();
+	
+		this.setVisible(true);
+	}
+	
+	private JButton createButton(String btnText){
+		JButton btn = new JButton();
+		btn.setText(btnText);
+		btn.addActionListener(this);
+		return btn;
+	}
+	
+	private JPanel createPanel(Color c) {
+		JPanel panel = new JPanel();
+		panel.setBackground(c);
+		return panel;
+	} 
+	
+	private void layoutButtonPanel() {
+		GridBagLayout layout = new GridBagLayout();
+		btnPanel.setLayout(layout);
+		//Lots of layout code here
+		//add components to grid
+		GridBagConstraints constraints = new GridBagConstraints();
+		//Defaults
+		constraints.fill = GridBagConstraints.NONE;
+		constraints.anchor = GridBagConstraints.CENTER;
+		constraints.weightx = 100;
+		constraints.weighty = 100;
+		
+		addToPanel(btnPanel, loadLogFileBTN,constraints,0,0,2,1);
+	} 
+	
+	/**
+	*
+	* A convenience method to add a component to given grid bag
+	* layout locations. Code due to Cay Horstmann
+	*
+	* @param c the component to add
+	* @param constraints the grid bag constraints to use
+	* @param x the x grid position
+	* @param y the y grid position
+	* @param w the grid width of the component
+	* @param h the grid height of the component
+	*/
+	private void addToPanel(JPanel jp,Component c, GridBagConstraints
+	constraints,int x, int y, int w, int h) {
+	constraints.gridx = x;
+	constraints.gridy = y;
+	constraints.gridwidth = w;
+	constraints.gridheight = h;
+	jp.add(c, constraints);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	    JFrame.setDefaultLookAndFeelDecorated(true);
+        SwingUtilities.invokeLater(new PizzaGUI("BorderLayout"));
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Object src = e.getSource();
+		if (src==loadLogFileBTN){
+			int returnVal = chooser.showOpenDialog(this);
+		    if(returnVal == JFileChooser.APPROVE_OPTION) {
+		       System.out.println("You chose to open this file: " +
+		            chooser.getSelectedFile().getName());
+		    }
+		}
+	}
 	
 	@Override
 	public void run() {
 		// TO DO
+		createGUI();
 	}
 
 	
