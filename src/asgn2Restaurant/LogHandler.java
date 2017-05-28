9371ebe3504d8ed9a0bc438f7e6e1eda9c67c5ce
PoeticIron <nmestone@gmail.com>
2017-05-28 22:12:34 +1000

    Modifications to LogHandler

--- src/asgn2Restaurant/LogHandler.java
+++ src/asgn2Restaurant/LogHandler.java
@@ -1,9 +1,5 @@
 package asgn2Restaurant;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -67,19 +63,13 @@ public class LogHandler {
 	 * 
 	 */
 	public static ArrayList<Pizza> populatePizzaDataset(String filename) throws PizzaException, LogHandlerException{
-<<<<<<< HEAD
 		// add in the pizza exception stuff.
-=======
->>>>>>> origin/master
+
 		BufferedReader in = null;
 		String textLine;
 		ArrayList<Pizza> pizzas = new ArrayList<Pizza>();
 		String line;
 		
-<<<<<<< HEAD
-		
-=======
->>>>>>> origin/master
 		try {
 			in = new BufferedReader(new FileReader(filename));
 			while((line = in.readLine()) != null) {
@@ -115,10 +105,6 @@ public class LogHandler {
 			throw new LogHandlerException("There was a problem parsing the line from the log file:" + ex);
 		}
 		return CustomerFactory.getCustomer(values[4], values[2], values[3], Integer.parseInt(values[5]), Integer.parseInt(values[6]));
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
 	}
 	
 	/**
@@ -130,43 +116,30 @@ public class LogHandler {
 	 * @throws LogHandlerException - If there was a problem parsing the line from the log file.
 	 */
 	public static Pizza createPizza(String line) throws PizzaException, LogHandlerException{
-<<<<<<< HEAD
 		//need to add pizzaException semantic errors.
-=======
 		// TO DO
-		//need to add pizzaException semantic erros.
->>>>>>> origin/master
+
+
 		LocalTime orderTime, deliveryTime;
 		int quantity;
 		String pizzaCode;
-		
 		String[] values;
 		try{
-<<<<<<< HEAD
 			values = line.split(",");
 		} catch(Exception ex){
 			throw new LogHandlerException("There was a problem parsing the line from the log file:" + ex);
 		}
 		System.out.println(values[0]);
-=======
-			values = line.split(", ");
-		} catch(Exception ex){
-			throw new LogHandlerException("There was a problem parsing the line from the log file:" + ex);
-		}
-		
->>>>>>> origin/master
+
 		orderTime = LocalTime.parse(values[0]);
 		deliveryTime = LocalTime.parse(values[1]);
 		quantity = Integer.parseInt(values[8]);
 		pizzaCode = values[7];
-		
-<<<<<<< HEAD
-=======
+
 		if(pizzaCode != "PZM" || pizzaCode != "PZV" || pizzaCode != "PZL"){
 			throw new PizzaException("invalid pizza code in line.");
 		}
 		
->>>>>>> origin/master
 		return PizzaFactory.getPizza(pizzaCode, quantity, orderTime, deliveryTime);
 	}
 
--- src/asgn2Tests/PizzaTests.java
+++ src/asgn2Tests/PizzaTests.java
@@ -1,17 +1,17 @@
 package asgn2Tests;
 
-import static org.junit.Assert.*;
-
 import java.time.LocalTime;
 
 import org.junit.Before;
-import org.junit.Test;
 
+import asgn2Exceptions.CustomerException;
+import asgn2Exceptions.LogHandlerException;
 import asgn2Exceptions.PizzaException;
 import asgn2Pizzas.MargheritaPizza;
 import asgn2Pizzas.MeatLoversPizza;
-import asgn2Pizzas.Pizza;
 import asgn2Pizzas.VegetarianPizza;
+import asgn2Restaurant.PizzaRestaurant;
+
 
 /**
  * A class that that tests the asgn2Pizzas.MargheritaPizza, asgn2Pizzas.VegetarianPizza, asgn2Pizzas.MeatLoversPizza classes. 
@@ -22,20 +22,19 @@ import asgn2Pizzas.VegetarianPizza;
  *
  */
 public class PizzaTests {
-	// TO DO
-	MargheritaPizza margherita;
-	VegetarianPizza vegetarian;
-	MeatLoversPizza meatLovers;
 	
-	@Before
-	public void beforeStuff() throws PizzaException{
-		margherita = new MargheritaPizza(1, LocalTime.parse("19:00:00"), LocalTime.parse("19:20:00"));
-		vegetarian = new VegetarianPizza(2, LocalTime.parse("20:00:00"), LocalTime.parse("20:25:00"));
-		meatLovers = new MeatLoversPizza(1, LocalTime.parse("21:00:00"),LocalTime.parse("21:35:00"));
-	}
+    MargheritaPizza margherita;
+    VegetarianPizza vegetarian;
+    MeatLoversPizza meatLovers;
+    PizzaRestaurant pizzaRestaurant;
+    
+    @Before
+    public void beforeStuff() throws PizzaException, CustomerException, LogHandlerException{
+        margherita = new MargheritaPizza(1, LocalTime.parse("19:00:00"), LocalTime.parse("19:20:00"));
+        vegetarian = new VegetarianPizza(2, LocalTime.parse("20:00:00"), LocalTime.parse("20:25:00"));
+        meatLovers = new MeatLoversPizza(1, LocalTime.parse("21:00:00"),LocalTime.parse("21:35:00"));
+        pizzaRestaurant = new PizzaRestaurant();
+        pizzaRestaurant.processLog("20170101.txt");
+    }
 	
-	@Test
-	public void dungive(){
-		assertEquals(margherita.getPizzaType(), "Margherita");
-	}
 }
